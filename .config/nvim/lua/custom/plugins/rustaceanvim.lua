4ef221c0bb3e19c6c25e8073869216ce2b0708d6
Austin Schey <aschey13@gmail.com>
2024-08-27 21:57:12 -0700

    update nvim

--- .config/nvim/lua/custom/plugins/rustaceanvim.lua
+++ .config/nvim/lua/custom/plugins/rustaceanvim.lua
@@ -2,4 +2,30 @@ return {
     "mrcjkb/rustaceanvim",
     version = "^5",
     lazy = false,
+    config = function()
+        vim.g.rustaceanvim = {
+            dap = {
+                autoload_configurations = false,
+            },
+            server = {
+                default_settings = {
+                    ["rust-analyzer"] = {
+                        cargo = {
+                            buildScripts = {
+                                enable = true,
+                            },
+                            features = "all",
+                        },
+                        checkOnSave = {
+                            enable = true,
+                        },
+                        check = {
+                            features = "all",
+                            command = "clippy",
+                        },
+                    },
+                },
+            },
+        }
+    end,
 }
