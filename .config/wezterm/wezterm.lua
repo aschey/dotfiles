0f2ff4980a2d871e9f309b9d1b87d930af5e054e
Austin Schey <aschey13@gmail.com>
2024-12-01 23:55:18 -0800

    updates

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,9 +1,18 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
-return {
+local config = {
 	font = wezterm.font("CaskaydiaCove Nerd Font Mono"),
 	color_scheme = "Catppuccin Mocha",
 	enable_wayland = true,
+	use_fancy_tab_bar = false,
+	tab_max_width = 32,
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+	status_update_interval = 500,
 	leader = { key = "a", mods = "CTRL" },
 	keys = {
 		{
@@ -39,6 +48,11 @@ return {
 			}),
 		},
 		{
+			key = "w",
+			mods = "CTRL|ALT",
+			action = act.CloseCurrentPane({ confirm = true }),
+		},
+		{
 			key = "LeftArrow",
 			mods = "LEADER",
 			action = act.AdjustPaneSize({ "Left", 5 }),
@@ -62,9 +76,27 @@ return {
 			key = "n",
 			mods = "CTRL|ALT",
 			action = wezterm.action.SpawnCommandInNewTab({
-				cwd =  "/home/aschey/.config/nvim" ,
+				cwd = "/home/aschey/.config/nvim",
 				args = { "nvim", "." },
 			}),
 		},
 	},
 }
+
+--local modal = wezterm.plugin.require("https://github.com/MLFlexer/modal.wezterm")
+--modal.apply_to_config(config)
+local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
+tabline.setup({})
+local tabline_config = require("tabline.config")
+config.colors = {
+	tab_bar = {
+		background = tabline_config.colors.normal_mode.c.bg,
+		new_tab = {
+			bg_color = tabline_config.colors.normal_mode.c.bg,
+			fg_color = tabline_config.colors.normal_mode.c.fg,
+		},
+	},
+}
+--tabline.apply_to_config(config)
+
+return config
